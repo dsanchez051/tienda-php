1639c4551c44303d91358a2bd74d2d67e29c00ad
Diego Sánchez <diegosanchezmazuela13@gmail.com>
2024-02-22 18:17:13 +0100

    index.php y navbar creado

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php require "partials/header.php" ?>
+
+<div class="welcome d-flex align-items-center justify-content-center">
+    <div class="text-center">
+        <h1>Welcome to Our Store</h1>
+        <p>Explore our wide range of products and start your shopping journey today!</p> 
+        <a class="btn btn-lg btn-dark" href="register.php">Start Shopping</a>
+    </div>
+</div>
+
+<?php require "partials/footer.php" ?>
